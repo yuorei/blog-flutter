865126d5cc3ffea99d4ee999407f73d7ad999c39
yuorei <yuorei71@gmail.com>
2023-07-18 19:04:54 +0900

    getでマークダウンの表示成功

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,105 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BlogApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class BlogApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ブログアプリ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ArticleListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class ArticleListPage extends StatefulWidget {
+  @override
+  _ArticleListPageState createState() => _ArticleListPageState();
+}
+
+class _ArticleListPageState extends State<ArticleListPage> {
+  List<dynamic> articles = [];
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void initState() {
+    super.initState();
+    fetchArticles();
   }
 
-  void _callAPI() async {
-    var url = Uri.parse(
-      'https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m&timezone=Asia%2FTokyo',
-    );
-    var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+  Future<void> fetchArticles() async {
+    final response = await http.get(Uri.parse('http://localhost:8080/foo'));
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      if (jsonData is List<dynamic>) {
+        setState(() {
+          articles = jsonData;
+        });
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          Icon(Icons.add),
-          Icon(Icons.share),
-        ],
+        title: Text('記事一覧'),
       ),
-      body: Center(
+      body: ListView.builder(
+        itemCount: articles.length,
+        itemBuilder: (context, index) {
+          final article = articles[index];
+          return ListTile(
+            title: Text(article['title']),
+            subtitle: Text(article['date']),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ArticleDetailPage(article: article),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ArticleDetailPage extends StatelessWidget {
+  final dynamic article;
+
+  ArticleDetailPage({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article['title']),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              'You have pushed the button this many times:',
+              article['date'],
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              onPressed: _callAPI,
-              child: const Text('Call API'),
+            SizedBox(height: 8),
+            MarkdownBody(
+              data: article['content'],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
